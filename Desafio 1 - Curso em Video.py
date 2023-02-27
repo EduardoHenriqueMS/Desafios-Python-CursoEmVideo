e298f5fa5c63123e17b7784280a256fb94f6cbe8
EduardoHenriqueMS <126426078+EduardoHenriqueMS@users.noreply.github.com>
2023-02-27 17:45:21 -0300

    Add files via upload

---
+++ Desafio 1 - Curso em Video.py
@@ -0,0 +1,7 @@
+# Crie um Script Python que leia o nome de uma pessoa e mostre
+# uma mensagem de boas-vindas de acordo com o valor digitado
+
+
+nome = input('Digite o seu nome: ')
+
+print('Bem-vindo ' + nome + '!'+ ' Prazer em te conhecer!')
---
+++ Desafio 3 - Curso em Video.py
@@ -0,0 +1,8 @@
+# Crie um script Python que leia dois números e tente 
+# mostrar a soma deles
+
+numero_1 = float(input('Digite um número: '))
+numero_2 = float(input('Digite um outro número: '))
+
+
+print('A soma desses números é: ', numero_1 + numero_2)
