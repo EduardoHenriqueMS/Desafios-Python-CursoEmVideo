6ed88bd53f7d6724fbb82996c4eccc6c83222fac
EduardoHenriqueMS <126426078+EduardoHenriqueMS@users.noreply.github.com>
2023-03-03 17:24:48 -0300

    Add files via upload

---
+++ Desafio 003 - Curso em Video.py
@@ -0,0 +1,9 @@
+# Crie um script Python que leia dois números e tente
+# mostrar a soma deles
+
+numero1 = int(input('Digite um número:'))
+numero2 = int(input('Digite um outro número:'))
+numero3 = numero1 + numero2
+
+print('A soma entre {} e {} é {}'.format(numero1, numero2, numero3))
+
