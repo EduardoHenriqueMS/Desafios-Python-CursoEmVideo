7869fbb4ea7177c071119edf2d2320f75958c377
EduardoHenriqueMS <126426078+EduardoHenriqueMS@users.noreply.github.com>
2023-03-03 17:23:21 -0300

    Delete Desafio 5 - Curso em Video.py

--- Desafio 5 - Curso em Video.py
+++
@@ -1,9 +0,0 @@
-# Faça um programaque leia um número inteiro e mostre na tela
-# o seu sucessor e o seu antecessor.
-
-numero1 = int(input('Digite um número:'))
-
-sucessor = numero1 + 1
-antecessor = numero1 - 1
-
-print('\n O antecessor do número {} é {} \n O sucessor do número {} é {}'.format(numero1,sucessor,numero1,antecessor))
