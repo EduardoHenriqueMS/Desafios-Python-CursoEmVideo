ba2c9753fa9ef6d1e4cc5a3e326ead22337a1c2c
EduardoHenriqueMS <126426078+EduardoHenriqueMS@users.noreply.github.com>
2023-03-03 17:16:05 -0300

    Add files via upload

---
+++ Desafio 10 - Curso em Video.py
@@ -0,0 +1,11 @@
+# Crie um programa que leia quanto dinheiro uma pessoa tem na
+# carteira e mostre quantos dólares ela pode comprar.
+# Considere -> US$ 1,00 = R$ 5,20
+
+cart = float(input('Informe quanto dinheiro você tem disponível em reais:'))
+
+dol = cart / 5.20
+
+print('A quantia de R$ {} pode proporcionar um valor de US$ {}'.format(cart,dol))
+
+
