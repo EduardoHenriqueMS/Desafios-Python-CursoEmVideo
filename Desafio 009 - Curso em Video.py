1480dc80b3b9887bd44e71cf8d243a62c1b4b058
EduardoHenriqueMS <126426078+EduardoHenriqueMS@users.noreply.github.com>
2023-03-04 13:45:54 -0300

    Update Desafio 009 - Curso em Video.py

--- Desafio 009 - Curso em Video.py
+++ Desafio 009 - Curso em Video.py
@@ -16,3 +16,17 @@ tab10 = num * 10
 
 print('\n {} \n {} \n {} \n {} \n {} \n {} \n {} \n {} \n {} \n {}'.format(tab1,tab2,tab3,tab4,tab5,tab6,tab7,tab8,tab9,tab10))
 
+# Outra variação que eu fiz mais formatada
+
+num = int(input('Digite um número:'))
+
+print('{} x {} = {}'.format(num, 1, num * 1))
+print('{} x {} = {}'.format(num, 2, num * 2))
+print('{} x {} = {}'.format(num, 3, num * 3))
+print('{} x {} = {}'.format(num, 4, num * 4))
+print('{} x {} = {}'.format(num, 5, num * 5))
+print('{} x {} = {}'.format(num, 6, num * 6))
+print('{} x {} = {}'.format(num, 7, num * 7))
+print('{} x {} = {}'.format(num, 8, num * 8))
+print('{} x {} = {}'.format(num, 9, num * 9))
+print('{} x {} = {}'.format(num, 10,num * 10))
