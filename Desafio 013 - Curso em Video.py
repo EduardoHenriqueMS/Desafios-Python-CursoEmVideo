5622c446ac86e81f79ca1907dea0463445951d0d
EduardoHenriqueMS <126426078+EduardoHenriqueMS@users.noreply.github.com>
2023-03-04 13:51:45 -0300

    Update Desafio 013 - Curso em Video.py

--- Desafio 013 - Curso em Video.py
+++ Desafio 013 - Curso em Video.py
@@ -5,4 +5,4 @@ salario = float(input('Digite o seu salário:'))
 
 aumen = salario * (115/100)
 
-print('O seu salário é {} e o seu novo salário passará a ser {}'.format(salario, aumen))
+print('O seu salário é {} e o seu novo salário passará a ser {:.2f}'.format(salario, aumen))
