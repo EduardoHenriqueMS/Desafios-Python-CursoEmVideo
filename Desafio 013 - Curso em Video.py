2984e103d4894920e5d9c734f93a58fed1825da2
EduardoHenriqueMS <126426078+EduardoHenriqueMS@users.noreply.github.com>
2023-03-03 22:52:01 -0300

    Add files via upload

---
+++ Desafio 013 - Curso em Video.py
@@ -0,0 +1,8 @@
+# Faça um algoritmo que leia o salário de um funcionário
+# e mostre seu novo salário, com 15% de aumento.
+
+salario = float(input('Digite o seu salário:'))
+
+aumen = salario * (115/100)
+
+print('O seu salário é {} e o seu novo salário passará a ser {}'.format(salario, aumen))
