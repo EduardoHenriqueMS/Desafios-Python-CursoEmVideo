c899cfde1a0ad659d6f65f954c87349f7ea0ae48
EduardoHenriqueMS <126426078+EduardoHenriqueMS@users.noreply.github.com>
2023-03-04 19:52:32 -0300

    Add files via upload

---
+++ Desafio 014 - Curso em Video.py
@@ -0,0 +1,11 @@
+# Escreva um programa que converta uma temperatura digitada em °C e converta para °F.
+
+celsius = float(input('Escreva a temperatura em graus celsius:'))
+
+faren = (celsius * 9/5) + 32
+
+print('A temperatura de {:.2f}°C é {:.2f}°F'.format(celsius, faren))
+
+
+
+
