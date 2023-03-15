a040fc598fb7ff3c2a4e096ffee4d46656396762
EduardoHenriqueMS <126426078+EduardoHenriqueMS@users.noreply.github.com>
2023-03-15 17:51:45 -0300

    Add files via upload

---
+++ Desafio 033 - Curso em Video.py
@@ -0,0 +1,25 @@
+# Faça um programa que leia três números e mostre qual é o MAIOR e qual é o MENOR.
+
+n1 = int(input('Digite o primeiro número: '))
+n2 = int(input('Digite o segundo número: '))
+n3 = int(input('Digite o terceiro número: '))
+
+menor = n1
+
+if n2<n1 and n2<n3:
+    menor = n2
+
+elif n3<n1 and n3<n2:
+    menor = n3
+
+print('O menor valor é: {}'.format(menor))
+
+maior = n3
+
+if n1>n2 and n1>n3:
+    maior = n1
+
+elif n2>n1 and n2>n3:
+    maior = n2
+
+print('O maior valor é: {}'.format(maior))
